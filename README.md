7ce71f665bea26b2a3875c199948d5eb9b1428ef
Arafat Rahman <opurahman@gmail.com>
2013-11-22 18:59:15 +0800

    initial

---
+++ README.md
@@ -0,0 +1,19 @@
+meteor-linkedin-reloaded
+============================
+
+A meteor package for LinkedIn's login service.
+
+Package Dependencies
+----------------------
+
+* accounts-base
+* accounts-oauth
+* linkedin
+
+Install
+-----------
+```
+mrt install linkedin-reloaded
+```
+
+```mrt``` is a Meteorite command line tool. Visit [Meteorite's page](http://oortcloud.github.com/meteorite/) to learn more.
---
+++ package.js
@@ -0,0 +1,20 @@
+Package.describe({
+  summary: "Accounts service for LinkedIn accounts"
+});
+
+Package.on_use(function(api) {
+  api.use('accounts-base', ['client', 'server']);
+  api.use('accounts-oauth', ['client', 'server']);
+  api.use('linkedin', ['client', 'server']);
+
+  // v0.6.5
+  // Export Accounts (etc) to packages using this one.
+  api.imply('accounts-base', ['client', 'server']);
+  api.use('linkedin', ['client', 'server']);
+
+  api.add_files(['linkedin_login_button.css'], 'client');
+
+  api.add_files('linkedin_common.js', ['client', 'server']);
+  api.add_files('linkedin_server.js', 'server');
+  api.add_files('linkedin_client.js', 'client');
+});
